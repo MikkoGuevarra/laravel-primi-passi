104fd1c60b0ae71d613ddc788b1eea2dcecab0cd
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-12-21 23:24:03 +0800

    hello world

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    </head>
+    <body>
+        <div class="container">
+            <h1>Hello World</h1>
+            <h2>I am learning Laravel</h2>
+
+        </div>
+    </body>
+</html>
